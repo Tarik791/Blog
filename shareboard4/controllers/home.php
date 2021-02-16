587771013c53eb853ceb8e5bbfbd5231b9c93319
Tarik791 <72981197+Tarik791@users.noreply.github.com>
2021-02-16 18:11:04 +0100

    Delete shareboard4 directory

--- shareboard4/controllers/home.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Class for Home
-class Home extends Controller{
-	protected function Index(){
-		//Calling HomeModel class
-		$viewmodel = new HomeModel();
-		$this->returnView($viewmodel->Index(), true);
-	}
-}
